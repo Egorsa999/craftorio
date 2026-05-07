e7a2bac0f81706127c1a2ec772d25d6487d056bc
Yahor Yasinski <153857791+Ezeg0r@users.noreply.github.com>
2026-05-07 20:13:14 +0200

    Now camera stops at the world boundaries

--- core/src/main/java/io/github/craftorio/model/Player.java
+++ core/src/main/java/io/github/craftorio/model/Player.java
@@ -1,13 +1,28 @@
 package io.github.craftorio.model;
 
+
+import com.badlogic.gdx.math.MathUtils;
+
 public class Player {
-    public float playerX = 0;
-    public float playerY = 0;
+
+    WorldMap worldMap;
+
+    public float playerX;
+    public float playerY;
 
     public static final float speed = 8;
+
+    public Player(WorldMap worldMap){
+        this.worldMap = worldMap;
+        playerX = worldMap.getWidth() / 2f;
+        playerY = worldMap.getHeight() / 2f;
+    }
 
     public void updatePosition(float delta, float dx, float dy){
         playerX += dx * speed * delta;
         playerY += dy * speed * delta;
+
+        playerX = MathUtils.clamp(playerX, 0.5f, worldMap.getWidth() - 0.5f);
+        playerY = MathUtils.clamp(playerY, 0.5f, worldMap.getHeight() - 0.5f);
     }
 }
