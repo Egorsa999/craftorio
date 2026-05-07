cd3740bbac168d000a42ec8241180a310198c078
egorsa999 <egorsavitski3020@gmail.com>
2026-05-07 20:57:04 +0200

    fixed player diagonal speed

--- core/src/main/java/io/github/craftorio/model/Player.java
+++ core/src/main/java/io/github/craftorio/model/Player.java
@@ -19,8 +19,8 @@ public class Player {
     }
 
     public void updatePosition(float delta, float dx, float dy){
-        playerX += dx * speed * delta;
-        playerY += dy * speed * delta;
+        playerX += (float) (dx * speed * delta / (!(dx == 0 || dy == 0) ? Math.sqrt(2) : 1));
+        playerY += (float) (dy * speed * delta / (!(dx == 0 || dy == 0) ? Math.sqrt(2) : 1));
 
         playerX = MathUtils.clamp(playerX, 0.5f, worldMap.getWidth() - 0.5f);
         playerY = MathUtils.clamp(playerY, 0.5f, worldMap.getHeight() - 0.5f);
