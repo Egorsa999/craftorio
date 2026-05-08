4a5cf065914b1b958e16ada512fe61e1c4470df1
egorsa999 <egorsavitski3020@gmail.com>
2026-05-08 13:08:20 +0200

    add Building class

---
+++ core/src/main/java/io/github/craftorio/model/building/Building.java
@@ -0,0 +1,35 @@
+package io.github.craftorio.model.building;
+
+public abstract class Building {
+    // bottom-left corner coordinates
+    private final int row;
+    private final int col;
+    // size of object
+    private final int width;
+    private final int height;
+
+    public Building(int row, int col, int width, int height) {
+        this.row = row;
+        this.col = col;
+        this.width = width;
+        this.height = height;
+    }
+
+    public abstract void update();
+
+    public int getRow() {
+        return this.row;
+    }
+
+    public int getCol() {
+        return this.col;
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
+
+    public int getWidth() {
+        return this.width;
+    }
+}
