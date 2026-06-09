23a358785bc757aa869efd48050a81ec0a83839f
Yahor Yasinski <153857791+Ezeg0r@users.noreply.github.com>
2026-06-09 23:17:01 +0200

    changed based resources

--- core/src/main/java/io/github/craftorio/GameScreen.java
+++ core/src/main/java/io/github/craftorio/GameScreen.java
@@ -100,8 +100,9 @@ public class GameScreen implements Screen {
         WorldMap worldMap = new WorldMap(GameConfig.WORLD_SIZE_WIDTH, GameConfig.WORLD_SIZE_HEIGHT);
         buildingRegistry = new BuildingRegistry();
         Inventory inventory = new Inventory();
-        inventory.add(ItemType.COPPER_ORE, 10000);
-        inventory.add(ItemType.IRON_ORE, 10000);
+        inventory.add(ItemType.COPPER_ORE, 100);
+        inventory.add(ItemType.IRON_ORE, 100);
+        inventory.add(ItemType.STONE, 100);
 
         Point spawnPoint = worldMap.findSpawnPoint();
         Player player = new Player(worldMap, buildingRegistry, spawnPoint, inventory);
